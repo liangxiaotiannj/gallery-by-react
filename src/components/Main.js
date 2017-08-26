55c9d682165634b219c941e0ec7a4724d8b28eb7
梁啸天 <liangxiaotian@liangxiaotiandeMacBook-Air.local>
2017-08-26 17:29:33 +0800

    init project

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,34 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+// let yeomanImage = require('../images/yeoman.png');
+
+let imageDatas = require('../data/imageData.json');
+
+imageDatas = (function genImageURl(imageDataArr) {
+  for (let i = 0, j = imageDataArr.length; i < j; i++) {
+    let singleImageData = imageDataArr[i];
+
+    singleImageData.imageURL = require('../images/' + singleImageData.filename);
+    imageDataArr[i] = singleImageData;
+  }
+
+  return imageDataArr;
+})(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="img-sec"></section>
+        <nav className="controller-nav"></nav>
+      </section>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
